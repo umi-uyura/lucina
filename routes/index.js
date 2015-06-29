61486e689ca1affc91113bdf612baba3d371c53b
Umi Uyura <umiuyura@gmail.com>
2015-06-29 18:37:32 +0900

    設定のチェック処理を追加

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,43 @@ var router = express.Router();
 
 var check = require('lib/birthcheck').check;
 
+var checkConfiguration = function() {
+  if (process.env.CHECK_URL === '') {
+    var err = new Error('Illegal configutation');
+    err.status = 500;
+    return err;
+  }
+
+  return null;
+};
+
 router.get('/', function(req, res, next) {
+  var ret = checkConfiguration();
+  if (ret) {
+    next(ret);
+    return;
+  }
+
   res.render('index',
              {
-               title: 'Express',
+               title: 'Lucina',
                url: process.env.CHECK_URL,
                interval: process.env.CHECK_INTERVAL
              });
 });
 
 router.post('/', function(req, res, next) {
+  var ret = checkConfiguration();
+  if (ret) {
+    next(ret);
+    return;
+  }
+
   check(process.env.CHECK_URL,
         function() {
           res.render('index',
                      {
-                       title: 'Express POST',
+                       title: 'Lucina result',
                        url: process.env.CHECK_URL,
                        interval: process.env.CHECK_INTERVAL
                      });
